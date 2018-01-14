f2d622f784472c92f6b5cef105a8a405db5ea108
Yanick <yankaelin@hotmail.com>
2018-01-14 01:33:46 +0100

    Space Shooter

--- Game/src/main/java/ch/lebois/game/GameScreen.java
+++ Game/src/main/java/ch/lebois/game/GameScreen.java
@@ -4,16 +4,22 @@ import java.awt.*;
 
 public class GameScreen implements SuperStateMachine{
 
-    public void update(double delta){
+    private Player player;
 
+    public GameScreen(){
+        player = new Player(150, 150, 50, 50);
+    }
+
+    public void update(double delta){
+        player.update(delta);
     }
 
     public void draw (Graphics2D g){
-
+        player.draw(g);
     }
 
     public void init(Canvas canvas){
-
+        canvas.addKeyListener(player);
     }
 }
 
